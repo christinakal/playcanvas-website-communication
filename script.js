8fb14fdd1e78725df7b888901ae8b81275551594
Christina Kaliora <christina.kaliora@pirron.one>
2020-08-12 15:33:54 +0200

    ui is updating on click

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-let iframeContent;
+
 let iframeReady = false;
 let messagesQueue = [];
 let data = {};
 
 function connectToIframe() {
-    iframeContent = document.getElementsByClassName("playcanvas-iframe").contentWindow;
-
     // --- postMessage event handlers
     window.addEventListener("message", parseMessage);
 }
 
 
 function parseMessage(event) {
-    if (!event.data) return;
+    if (!event || !event.data) return;
     data = event.data;
 
     // update text elements
+    const iframe = document.getElementById("playcanvas-iframe");
+    removeClass();
     updateText(data);
 }
 
-
 function updateText(dataFromPC){
+
     console.log('dataFromPC', dataFromPC);
+    
     const texture = document.getElementById("texture-name");
     texture.innerHTML = dataFromPC.texture;
 
     const modelType = document.getElementById("model-type");
     modelType.innerHTML = dataFromPC.modelType;
+
+    
+
 }
+
+function removeClass(){
+    const textureText = document.getElementById('texture-name-parent');
+    textureText.classList.remove("test-name");
+
+    const modelTypeText = document.getElementById("model-type-parent");
+    modelTypeText.classList.remove("test-name");
+}
+
+
+
 
 
 // -- execute
